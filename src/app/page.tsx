25a579d9f02bf13feacdd1d672b159125b7221a0
jordonv <j.vanpelt1993@gmail.com>
2025-11-11 21:54:35 -0500

    reset

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
-import { createSupabaseServerFromCookies } from '@lib/supabase';
-
-export default async function Home() {
-  const supabase = createSupabaseServerFromCookies(cookies());
-  const { data: { user } } = await supabase.auth.getUser();
-  redirect(user ? '/app' : '/login');}
