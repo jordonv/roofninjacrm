c29f4c3c1721dc3932e14e04fd3636956243a515
jordonv <j.vanpelt1993@gmail.com>
2025-11-11 21:02:01 -0500

    Refactor RootLayout function parameter syntax

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,7 @@ export const metadata = {
   description: 'Roof Ninja CRM'
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body style={{ margin: 0, fontFamily: 'system-ui, -apple-system, Segoe UI, Roboto, Helvetica, Arial' }}>
